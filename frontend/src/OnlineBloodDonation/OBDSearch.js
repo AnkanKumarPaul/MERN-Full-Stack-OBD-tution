6e94ae4897932610b82bfb8d4500930552347317
ANKAN KUMAR PAUL <144710026+AnkanKumarPaul@users.noreply.github.com>
2024-07-29 14:48:27 +0530

    Update OBDSearch.js

--- frontend/src/OnlineBloodDonation/OBDSearch.js
+++ frontend/src/OnlineBloodDonation/OBDSearch.js
@@ -5,7 +5,7 @@ import "./OBDSearching.css"
 
 function OBDSearch() {
 
-    const [alldonors, setAlldonors] = useState([]) 
+    const [alldonors, setAlldonors] = useState([])
     const [alluser, setAlluser] = useState([])
     const [address, setAddress] = useState([])
     const [bloodgroup, setBloodgroup] = useState([])
@@ -14,13 +14,15 @@ function OBDSearch() {
     const navigate = useNavigate();
 
     const getData = async () => {
-        const response = await fetch('https://mern-full-stack-obd-tution-backend.onrender.com/getAlldonor');
+        // const email = localStorage.getItem('loggedDonor')
+        const response = await fetch('http://localhost:5000/getAlldonor');
+        // const response = await fetch('http://localhost:5000/getDonerByEmail/' + email)
         const data = await response.json();
         setAlldonors(data)
     }
 
     const getDataofuser = async () => {
-        const response = await fetch('https://mern-full-stack-obd-tution-backend.onrender.com/getAlluser');
+        const response = await fetch('http://localhost:5000/getAlluser');
         const data = await response.json();
         setAlluser(data)
     }
@@ -49,7 +51,7 @@ function OBDSearch() {
                 body: JSON.stringify(byaddressandblood)
             };
 
-            const response = await fetch('https://mern-full-stack-obd-tution-backend.onrender.com/searchbyboth', requestOptions)
+            const response = await fetch('http://localhost:5000/searchbyboth', requestOptions)
             const data1 = await response.json();
             setAlldonors(data1)
         }
@@ -58,7 +60,7 @@ function OBDSearch() {
 
 
     //ei part ta korchilam
-    // for blood group
+    // for blood group search donor
     const searchbybloodgroup = async (value) => {
         setBloodgroup(value)
         if (value == null || value === "") {
@@ -67,13 +69,34 @@ function OBDSearch() {
 
         else {
 
-            const response = await fetch(`https://mern-full-stack-obd-tution-backend.onrender.com/searchbybloodgroup/${value}`)
+            const response = await fetch(`http://localhost:5000/searchbybloodgroup/${value}`)
             const data1 = await response.json();
             setAlldonors(data1)
             console.log(data1);
         }
         // 
     }
+    // for blood group search donor
+
+    // for blood group search user
+
+    const searchbybloodgrouptwo = async (value) => {
+        setBloodgroup(value)
+        if (value == null || value === "") {
+            getData()
+        }
+
+        else {
+
+            const responsetwo = await fetch(`http://localhost:5000/searchbybloodgrouptwo/${value}`)
+
+            const data1 = await responsetwo.json();
+            setAlluser(data1)
+            console.log(data1);
+        }
+        // 
+    }
+    // for blood group search user
 
 
     const searchbyname = async (value) => {
@@ -84,7 +107,7 @@ function OBDSearch() {
 
         else {
 
-            const response = await fetch(`https://mern-full-stack-obd-tution-backend.onrender.com/searchbyname/${value}`)
+            const response = await fetch(`http://localhost:5000/searchbyname/${value}`)
             const datathree = await response.json();
             setName(datathree)
             console.log(datathree);
@@ -105,14 +128,14 @@ function OBDSearch() {
     return (
         <>
 
-            <h1 className="h1search">Online Blood Donation</h1>
+            <h1 className="h1search">Search Blood  </h1>
 
             <table className="tablesearch">
 
                 <tr>
                     <th className="tableheadsearch">Search Donor</th>
                 </tr>
-<br></br>
+                <br></br>
                 <tr>
                     <td >Select Blood Group <select onChange={(e) => searchbybloodgroup(e.target.value)} name="" id="" title="Blood Group" className="bloodgroup">
 
@@ -140,7 +163,7 @@ function OBDSearch() {
 
                     </td>
                 </tr>
-<br></br>
+                <br></br>
             </table>
 
             <br></br>
@@ -164,7 +187,7 @@ function OBDSearch() {
 
                         <th scope="col"><b>Email</b></th>
 
-                        <th scope="col"><b>Password</b></th>
+                        {/* <th scope="col"><b>Password</b></th> */}
 
                         <th scope="col"><b>Address</b></th>
 
@@ -189,7 +212,7 @@ function OBDSearch() {
 
                                 <td class="table-warning" >{data.email}</td>
 
-                                <td class="table-success">{data.password}</td>
+                                {/* <td class="table-success">{data.password}</td> */}
 
                                 <td class="table-danger">{data.address}</td>
 
@@ -207,8 +230,46 @@ function OBDSearch() {
             </table>
 
 
+            {/* <br></br> */}
+
+            <table className="tablesearch">
+
+                <tr>
+                    <th className="tableheadsearch">Search User</th>
+                </tr>
+                <br></br>
+                <tr>
+                    <td >Select Blood Group <select onChange={(e) => searchbybloodgrouptwo(e.target.value)} name="" id="" title="Blood Group" className="bloodgroup">
+
+                        <option >Select</option>
+                        <option value="a+">a+</option>
+                        <option >A-</option>
+                        <option >B+</option>
+                        <option >B-</option>
+                        <option >o+</option>
+                        <option >o-</option>
+
+                    </select>
+
+                    </td>
+                </tr>
+                <br></br>
+                <tr>
+                    <td>Enter Your Location:  <input onChange={(e) => searchbyboth(e.target.value)} type="search" placeholder="Enter Location" />
+
+                    </td>
+                </tr>
+                <br></br>
+                <tr>
+                    <td>Enter Your Name:  <input onChange={(e) => searchbyname(e.target.value)} type="search" placeholder="Enter Name" />
+
+                    </td>
+                </tr>
+                <br></br>
+            </table>
 
 
+            <br></br>
 
             <h1 className="donorslistname" >User's List</h1>
 
@@ -226,7 +287,7 @@ function OBDSearch() {
 
                         <th scope="col"><b>Email</b></th>
 
-                        <th scope="col"><b>Password</b></th>
+                        {/* <th scope="col"><b>Password</b></th> */}
 
                         <th scope="col"><b>Address</b></th>
 
@@ -257,7 +318,7 @@ function OBDSearch() {
 
                                 <td class="table-warning" >{data.email}</td>
 
-                                <td class="table-success">{data.password}</td>
+                                {/* <td class="table-success">{data.password}</td> */}
 
                                 <td class="table-danger">{data.address}</td>
 
