564cb7093f5744d3e5989774aa20391834a8e904
ANKAN KUMAR PAUL <144710026+AnkanKumarPaul@users.noreply.github.com>
2024-07-29 23:14:11 +0530

    Update OBDUserlogin.js

--- frontend/src/OnlineBloodDonation/OBDUserlogin.js
+++ frontend/src/OnlineBloodDonation/OBDUserlogin.js
@@ -70,6 +70,12 @@ function OBDUserlogin() {
         <>
 
             <h1 className="h1login">Online Blood Donation</h1>
+
+             <marquee width="100%">
+                <h5 className="forusertablemessagetwo">Login and Registration will take some time for first time,
+                    request you to please wait... :) </h5>
+            </marquee>
+        
             <table className="tablelogin">
                 <tr>
 
