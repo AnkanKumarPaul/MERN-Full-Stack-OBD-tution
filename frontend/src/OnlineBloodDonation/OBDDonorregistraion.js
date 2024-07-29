01d13edb31976208d9ebd839f86f8c307ebf2590
ANKAN KUMAR PAUL <144710026+AnkanKumarPaul@users.noreply.github.com>
2024-07-29 22:33:24 +0530

    Update OBDDonorregistraion.js

--- frontend/src/OnlineBloodDonation/OBDDonorregistraion.js
+++ frontend/src/OnlineBloodDonation/OBDDonorregistraion.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
 
 import "./OBDDonorregistraion.css"
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function OBDDonorregistraion() {
 
+    const [flag, setFlag] = useState(0)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [name, setName] = useState('')
@@ -38,14 +40,23 @@ function OBDDonorregistraion() {
         const data = await response.json();
 
         if (data._id != null) {
-            setMessage("Donor Registration Successfully")
-
+            setMessage("Donor Registration Successfull")
+            window.location.href = "/"
         }
         else {
             setMessage("Registraion Failed")
         }
     }
 
+
+    const showHide = (id) => {
+        if (flag == 0) {
+            setFlag(1)
+        }
+        else {
+            setFlag(0)
+        }
+    }
 
     return (
         <>
@@ -59,28 +70,47 @@ function OBDDonorregistraion() {
                 </tr>
                 <br></br>
                 <tr>
-                    <td>Enter Your Name <input type="text" name="" placeholder="Enter Name" onChange={(e) => setName(e.target.value)} /></td>
+                    <td>Enter Your Name : <input type="text" name="" placeholder="Enter Name" onChange={(e) => setName(e.target.value)} /></td>
                 </tr>
                 <br></br>
                 <tr>
-                    <td>Enter Email Id <input type="email" name="" placeholder="Enter Email Id" onChange={(e) => setEmail(e.target.value)} /></td>
+                    <td>Enter Email Id : <input type="email" name="" placeholder="Enter Email Id" onChange={(e) => setEmail(e.target.value)} /></td>
                 </tr>
                 <br></br>
-                <tr>
+
+                <tr><td>Enter Password :
+                    {
+                        flag == 0 ?
+                            <input className="my-input" type="password" name="" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
+
+                            :
+
+                            <input className="my-input" type="text" name="" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
+                    }
+
+                    <a onClick={showHide} className="donorloginshowhide">
+
+                        {flag == 0 ? <FaEyeSlash /> : <FaEye />}
+
+                    </a>
+
+
+                </td></tr>
+                {/* <tr>
                     <td>Enter Password <input className="my-input" type="password" name="" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} /></td>
+                </tr> */}
+                <br></br>
+                <tr>
+                    <td>Enter Your Address : <input type="text" name="" placeholder="Enter Address" onChange={(e) => setAddress(e.target.value)} /></td>
                 </tr>
                 <br></br>
                 <tr>
-                    <td>Enter Your Address <input type="text" name="" placeholder="Enter Address" onChange={(e) => setAddress(e.target.value)} /></td>
-                </tr>
-                <br></br>
-                <tr>
-                    <td>Enter Your Phone No <input type="number" name="" placeholder="Enter Phone No" onChange={(e) => setPhoneno(e.target.value)} /></td>
+                    <td>Enter Your Phone No : <input type="number" name="" placeholder="Enter Phone No" onChange={(e) => setPhoneno(e.target.value)} /></td>
                 </tr>
                 <br></br>
                 <tr>
 
-                    <td>Select Blood Group  <select name="" id="" title="Blood Group" onChange={(e) => setBloodgroup(e.target.value)}>
+                    <td>Select Blood Group : <select name="" id="" title="Blood Group" onChange={(e) => setBloodgroup(e.target.value)}>
 
                         <option >Select</option>
                         <option >A+</option>
@@ -97,12 +127,12 @@ function OBDDonorregistraion() {
                 <br></br>
 
                 {message}
-                
+
                 <tr >
                     <td className="registrationSwitch"><input className="registrationSwitchin" type="Submit" value="Save" onClick={registerDonor} /></td>
                 </tr>
                 <br></br>
-               
+
                 <br></br>
 
             </table>
