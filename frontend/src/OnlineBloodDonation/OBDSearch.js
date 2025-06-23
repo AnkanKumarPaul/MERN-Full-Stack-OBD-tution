561ca7f45fdba402b1e704d253ed032de16a5b7c
ANKAN KUMAR PAUL <144710026+AnkanKumarPaul@users.noreply.github.com>
2025-06-23 17:18:30 +0530

    Update OBDSearch.js

--- frontend/src/OnlineBloodDonation/OBDSearch.js
+++ frontend/src/OnlineBloodDonation/OBDSearch.js
@@ -16,15 +16,10 @@ function OBDSearch() {
     const [feedbackData, setFeedbackData] = useState([]);
     const navigate = useNavigate(); // Initialize navigate hook
 
-    useEffect(() => {
-    getData();
-}, []);
-
     
     const getData = async () => {
         const response = await fetch('https://mern-full-stack-obd-tution-backend.onrender.com/getAlldonor');
         const data = await response.json();
-        console.log("Donor data fetched:", data);
         setAlldonors(data);
     };
 
